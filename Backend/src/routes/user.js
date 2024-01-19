8b0c1a73a584f97644af9f37ac4e3c8ac8517b05
AngelSalas64 <61994987+AngelSalas64@users.noreply.github.com>
2024-01-19 14:57:48 -0600

    CRUD usuarios

--- Backend/src/routes/user.js
+++ Backend/src/routes/user.js
@@ -3,17 +3,41 @@ import user from "../models/user.js";
 
 const router = Router();
 
-const mainrut = (req, res) => {
-    res.send('hello zawarudo')
-};
-
 const getUsers = (req, res) => {
     user.find()
     .then((data) => res.json(data))
     .catch((error)=>{console.log(error)})
 };
 
-router.get('/', mainrut);
+const getUser = (req, res) => {
+    const { id } = req.params;
+    user.find({correo: `${id}`})
+    .then((data)=>{res.json(data[0])}).catch((error)=>{console.log(error)})
+};
+
+const createUser = (req, res) => {
+    const newusuario = new user(req.body);
+    newusuario.save()
+    .then(()=>{res.json("usuario creado con éxito")}).catch((error)=>{console.log(error)})
+};
+
+const updateUser = (req, res) => {
+    const { id } = req.params;
+    const usuario = new user(req.body);
+    user.findByIdAndUpdate(id, usuario)
+    .then(()=>{res.json("actualización exitosa")}).catch((error)=>{console.log(error)})
+};
+
+const deleteUser = (req, res) => {
+    const { id } = req.params;
+    user.findByIdAndDelete(id)
+    .then(()=>{res.json("borrado exitoso")}).catch((error)=>{console.log(error)})
+};
+
 router.get('/users', getUsers)
+router.get('/users/:id', getUser);
+router.post('/users', createUser);
+router.update('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
 
 export default router;
