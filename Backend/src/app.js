c04070416da9a78fb58a4643fdfa85d5f58257ca
Victor Rios Saucedo <36146644+Rotooo@users.noreply.github.com>
2024-02-04 12:20:04 -0600

    04/02/2024
    pequeños cambios en el frontend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,7 @@ import mongoose from "mongoose";
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/Verreti')
+mongoose.connect('mongodb+srv://user243:uMpCEIGxXvGm9iuh@verretidemo.i12evdn.mongodb.net/')
     .then((db)=>{console.log('db is connected')})
     .catch((error)=>{console.log(error, 'something went wrong')});
 
--- Frontend/src/app.js
+++ Frontend/src/app.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import InDexDB from './Context/IndexDB';
-import UserPage from './pages/ReportsSSIDPage';
+import HomePage from './pages/HomePage';
 
 export default function app() {
   return (
     <InDexDB>
       <div className='app'>
-        <UserPage />
+        <BrowserRouter>
+          <div className='pages'>
+            <Routes>
+              <Route path='/' element={<HomePage />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
       </div>
     </InDexDB>
   )
